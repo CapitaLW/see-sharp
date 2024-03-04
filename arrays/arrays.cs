6a3d7d13f8ad88b0ef2537f08f2f13a7ad47a06f
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-03 21:08:01 -0600

    modified:   Calculator.sln 	modified:   Calculator/bin/Debug/net8.0/Calculator.dll modified:   Calculator/bin/Debug/net8.0/Calculator.exe 	modified:   Calculator/bin/Debug/net8.0/Calculator.pdb 	modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs 	modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfoInputs.cache 	modified:   Calculator/obj/Debug/net8.0/Calculator.dll 	modified:   Calculator/obj/Debug/net8.0/Calculator.pdb 	modified:   Calculator/obj/Debug/net8.0/Calculator.sourcelink.json 	modified:   Calculator/obj/Debug/net8.0/apphost.exe 	modified:   Calculator/obj/Debug/net8.0/ref/Calculator.dll 	modified:   Calculator/obj/Debug/net8.0/refint/Calculator.dll 	new file:   arrays/arrays.cs 	new file:   arrays/arrays.csproj 	new file:   arrays/bin/Debug/net8.0/arrays.deps.json 	new file:   arrays/bin/Debug/net8.0/arrays.dll 	new file:   arrays/bin/Debug/net8.0/arrays.exe 	new file:   arrays/bin/Debug/net8.0/arrays.pdb 	new file:   arrays/bin/Debug/net8.0/arrays.runtimeconfig.json 	new file:   arrays/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   arrays/obj/Debug/net8.0/apphost.exe 	new file:   arrays/obj/Debug/net8.0/arrays.AssemblyInfo.cs 	new file:   arrays/obj/Debug/net8.0/arrays.AssemblyInfoInputs.cache 	new file:   arrays/obj/Debug/net8.0/arrays.GeneratedMSBuildEditorConfig.editorconfig 	new file:   arrays/obj/Debug/net8.0/arrays.GlobalUsings.g.cs 	new file:   arrays/obj/Debug/net8.0/arrays.assets.cache new file:   arrays/obj/Debug/net8.0/arrays.csproj.CoreCompileInputs.cache 	new file:   arrays/obj/Debug/net8.0/arrays.csproj.FileListAbsolute.txt 	new file:   arrays/obj/Debug/net8.0/arrays.dll 	new file:   arrays/obj/Debug/net8.0/arrays.genruntimeconfig.cache 	new file:   arrays/obj/Debug/net8.0/arrays.pdb 	new file:   arrays/obj/Debug/net8.0/arrays.sourcelink.json 	new file:   arrays/obj/Debug/net8.0/ref/arrays.dll 	new file:   arrays/obj/Debug/net8.0/refint/arrays.dll new file:   arrays/obj/arrays.csproj.nuget.dgspec.json 	new file:   arrays/obj/arrays.csproj.nuget.g.props 	new file:   arrays/obj/arrays.csproj.nuget.g.targets 	new file:   arrays/obj/project.assets.json 	new file:   arrays/obj/project.nuget.cache

---
+++ arrays/arrays.cs
@@ -0,0 +1,12 @@
+﻿// arrays are variables that hold other variables in a fixed ammount
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] cars = { "Bmw", "Temsla", "Telemetry" };
+        Console.WriteLine(cars[0]);
+        Console.WriteLine(cars[1]);
+        Console.WriteLine(cars[2]);
+    }
+}
