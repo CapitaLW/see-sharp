f551f50e5ccfe786b5c98b11c6b0e308b97bb7f3
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-03 19:53:31 -0600

    new file:   .vscode/settings.json 	new file:   Calculator.sln 	new file:   Calculator/Calculator.csproj 	new file:   Calculator/Program.cs 	new file:   Calculator/bin/Debug/net8.0/Calculator.deps.json 	new file:   Calculator/bin/Debug/net8.0/Calculator.dll 	new file:   Calculator/bin/Debug/net8.0/Calculator.exe 	new file:   Calculator/bin/Debug/net8.0/Calculator.pdb 	new file:   Calculator/bin/Debug/net8.0/Calculator.runtimeconfig.json 	new file:   Calculator/obj/Calculator.csproj.nuget.dgspec.json 	new file:   Calculator/obj/Calculator.csproj.nuget.g.props 	new file:   Calculator/obj/Calculator.csproj.nuget.g.targets 	new file:   Calculator/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs 	new file:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfoInputs.cache 	new file:   Calculator/obj/Debug/net8.0/Calculator.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Calculator/obj/Debug/net8.0/Calculator.GlobalUsings.g.cs 	new file:   Calculator/obj/Debug/net8.0/Calculator.assets.cache 	new file:   Calculator/obj/Debug/net8.0/Calculator.csproj.CoreCompileInputs.cache 	new file:   Calculator/obj/Debug/net8.0/Calculator.csproj.FileListAbsolute.txt 	new file:   Calculator/obj/Debug/net8.0/Calculator.dll 	new file:   Calculator/obj/Debug/net8.0/Calculator.genruntimeconfig.cache 	new file:   Calculator/obj/Debug/net8.0/Calculator.pdb 	new file:   Calculator/obj/Debug/net8.0/apphost.exe 	new file:   Calculator/obj/Debug/net8.0/ref/Calculator.dll 	new file:   Calculator/obj/Debug/net8.0/refint/Calculator.dll 	new file:   Calculator/obj/project.assets.json 	new file:   Calculator/obj/project.nuget.cache

---
+++ Calculator/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+       do{
+        //declare and init variables
+        double num1 = 0;
+        double num2 = 0;
+        double result = 0;
+
+
+        //title
+        Console.WriteLine("--------------");
+        Console.WriteLine(" Calc program ");
+        Console.WriteLine("--------------");
+
+
+        // input
+        Console.Write("Enter Number 1: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Number 2: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+// the '\t' is optional
+        Console.WriteLine("Enter an option:");
+        Console.WriteLine("\t+ : add");
+        Console.WriteLine("\t- : subtract");
+        Console.WriteLine("\t* : multiply");
+        Console.WriteLine("\t/ : Divide");
+// query for option
+        System.Console.Write("Enter an option: ");
+
+
+// determines what mathematical operation to perform!
+switch (Console.ReadLine())
+{
+    case "+":
+    result = num1 + num2;
+    Console.WriteLine("it's: " + result);
+    break;
+     case "-":
+    result = num1 - num2;
+    Console.WriteLine("it's: " + result);
+    break;
+     case "*":
+    result = num1 * num2;
+    Console.WriteLine("it's: " + result);
+    break;
+     case "/":
+    result = num1 / num2;
+    Console.WriteLine("it's: " + result);
+    break;
+// if none above are chosen, the default will perform!
+    default:
+    Console.WriteLine("Thats not a valid option");
+    break;
+}
+Console.Write("would you like to calculate again? (Y/N): ");
+
+       } while(Console.ReadLine().ToUpper() == "Y");
+
+Console.WriteLine("Bye!");
+
+        // less garble hopefully!
+             Console.ReadKey();
+    }
+}
+
+
