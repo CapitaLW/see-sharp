79a57f9a1be5fd5a350191b51ad82175a04d4070
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-03 20:02:15 -0600

    modified:   Calculator/Program.cs 	modified:   Calculator/bin/Debug/net8.0/Calculator.dll 	modified:   Calculator/bin/Debug/net8.0/Calculator.exe 	modified:   Calculator/bin/Debug/net8.0/Calculator.pdb 	modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs 	modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfoInputs.cache 	modified:   Calculator/obj/Debug/net8.0/Calculator.csproj.FileListAbsolute.txt 	modified:   Calculator/obj/Debug/net8.0/Calculator.dll 	modified:   Calculator/obj/Debug/net8.0/Calculator.pdb 	new file:   Calculator/obj/Debug/net8.0/Calculator.sourcelink.json 	modified:   Calculator/obj/Debug/net8.0/apphost.exe 	modified:   Calculator/obj/Debug/net8.0/ref/Calculator.dll 	modified:   Calculator/obj/Debug/net8.0/refint/Calculator.dll

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,69 +1,66 @@
-﻿internal class Program
+﻿
+do
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
 {
-    private static void Main(string[] args)
+    //declare and init variables
+    double num1 = 0;
+    double num2 = 0;
+    double result = 0;
+
+
+    //title
+    Console.WriteLine("--------------");
+    Console.WriteLine(" Calc program ");
+    Console.WriteLine("--------------");
+
+
+    // input
+    Console.Write("Enter Number 1: ");
+    num1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter Number 2: ");
+    num2 = Convert.ToDouble(Console.ReadLine());
+    // the '\t' is optional
+    Console.WriteLine("Enter an option:");
+    Console.WriteLine("\t+ : add");
+    Console.WriteLine("\t- : subtract");
+    Console.WriteLine("\t* : multiply");
+    Console.WriteLine("\t/ : Divide");
+    // query for option
+    System.Console.Write("Enter an option: ");
+
+
+    // determines what mathematical operation to perform!
+    switch (Console.ReadLine())
     {
-       
-       do{
-        //declare and init variables
-        double num1 = 0;
-        double num2 = 0;
-        double result = 0;
+        case "+":
+            result = num1 + num2;
+            Console.WriteLine("it's: " + result);
+            break;
+        case "-":
+            result = num1 - num2;
+            Console.WriteLine("it's: " + result);
+            break;
+        case "*":
+            result = num1 * num2;
+            Console.WriteLine("it's: " + result);
+            break;
+        case "/":
+            result = num1 / num2;
+            Console.WriteLine("it's: " + result);
+            break;
+        // if none above are chosen, the default will perform!
+        default:
+            Console.WriteLine("Thats not a valid option");
+            break;
+    }
+    Console.Write("would you like to calculate again? (Y/N): ");
 
-
-        //title
-        Console.WriteLine("--------------");
-        Console.WriteLine(" Calc program ");
-        Console.WriteLine("--------------");
-
-
-        // input
-        Console.Write("Enter Number 1: ");
-        num1 = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Enter Number 2: ");
-        num2 = Convert.ToDouble(Console.ReadLine());
-// the '\t' is optional
-        Console.WriteLine("Enter an option:");
-        Console.WriteLine("\t+ : add");
-        Console.WriteLine("\t- : subtract");
-        Console.WriteLine("\t* : multiply");
-        Console.WriteLine("\t/ : Divide");
-// query for option
-        System.Console.Write("Enter an option: ");
-
-
-// determines what mathematical operation to perform!
-switch (Console.ReadLine())
-{
-    case "+":
-    result = num1 + num2;
-    Console.WriteLine("it's: " + result);
-    break;
-     case "-":
-    result = num1 - num2;
-    Console.WriteLine("it's: " + result);
-    break;
-     case "*":
-    result = num1 * num2;
-    Console.WriteLine("it's: " + result);
-    break;
-     case "/":
-    result = num1 / num2;
-    Console.WriteLine("it's: " + result);
-    break;
-// if none above are chosen, the default will perform!
-    default:
-    Console.WriteLine("Thats not a valid option");
-    break;
-}
-Console.Write("would you like to calculate again? (Y/N): ");
-
-       } while(Console.ReadLine().ToUpper() == "Y");
+} while (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase));
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 
 Console.WriteLine("Bye!");
 
-        // less garble hopefully!
-             Console.ReadKey();
-    }
-}
+// less garble hopefully!
+Console.ReadKey();
 
 
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f551f50e5ccfe786b5c98b11c6b0e308b97bb7f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
