cc11c02f96990e04c93379aeaf58f5938676223d
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-04 00:48:18 -0600

    modified:   THE METHODS/Methodation.cs

--- THE METHODS/Methodation.cs
+++ THE METHODS/Methodation.cs
@@ -1,1 +1,19 @@
 ﻿// METHOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOODS. like main!, for example
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+
+
+
+
+        //dip
+        Console.ReadKey();
+    }
+}
