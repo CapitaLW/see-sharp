a7ed4555b05b95427b05877a20b71b49aed0a089
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-05 16:41:31 -0600

    modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs 	modified:   Calculator/obj/Debug/net8.0/Calculator.AssemblyInfoInputs.cache 	modified:   Calculator/obj/Debug/net8.0/Calculator.sourcelink.json

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db74de1bedd58f02210d4ea8e88b898d3b89e9d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164051323869169493e27c0df1791bf258e99fc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
