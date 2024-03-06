914e4730e1c56e7b66dfe81f65e840cf7a85285e
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-06 17:10:48 -0600

    modified:   THE METHODS/obj/Debug/net8.0/THE METHODS.GeneratedMSBuildEditorConfig.editorconfig 	modified:   THE METHODS/obj/Debug/net8.0/THE METHODS.assets.cache 	modified:   arrays/obj/Debug/net8.0/arrays.GeneratedMSBuildEditorConfig.editorconfig 	modified:   arrays/obj/Debug/net8.0/arrays.assets.cache 	new file:   dacalc/bin/Debug/net8.0/dacalc.deps.json 	new file:   dacalc/bin/Debug/net8.0/dacalc.dll 	new file:   dacalc/bin/Debug/net8.0/dacalc.exe 	new file:   dacalc/bin/Debug/net8.0/dacalc.pdb 	new file:   dacalc/bin/Debug/net8.0/dacalc.runtimeconfig.json modified:   dacalc/calc.cs 	new file:   dacalc/obj/Debug/net8.0/apphost.exe 	modified:   dacalc/obj/Debug/net8.0/dacalc.AssemblyInfo.cs 	modified:   dacalc/obj/Debug/net8.0/dacalc.AssemblyInfoInputs.cache 	new file:   dacalc/obj/Debug/net8.0/dacalc.assets.cache 	new file:   dacalc/obj/Debug/net8.0/dacalc.csproj.CoreCompileInputs.cache 	new file:   dacalc/obj/Debug/net8.0/dacalc.csproj.FileListAbsolute.txt 	new file:   dacalc/obj/Debug/net8.0/dacalc.dll 	new file:   dacalc/obj/Debug/net8.0/dacalc.genruntimeconfig.cache 	new file:   dacalc/obj/Debug/net8.0/dacalc.pdb 	new file:   dacalc/obj/Debug/net8.0/dacalc.sourcelink.json 	new file:   dacalc/obj/Debug/net8.0/ref/dacalc.dll 	new file:   dacalc/obj/Debug/net8.0/refint/dacalc.dll 	new file:   dacalc/obj/dacalc.csproj.nuget.dgspec.json 	new file:   dacalc/obj/dacalc.csproj.nuget.g.props 	new file:   dacalc/obj/dacalc.csproj.nuget.g.targets 	new file:   dacalc/obj/project.assets.json new file:   dacalc/obj/project.nuget.cache modified:   for each loops/obj/Debug/net8.0/for each loops.GeneratedMSBuildEditorConfig.editorconfig 	modified:   for each loops/obj/Debug/net8.0/for each loops.assets.cache

--- dacalc/calc.cs
+++ dacalc/calc.cs
@@ -1,65 +1,62 @@
-
-do
-//declare and initialize variables
+﻿//declare and initialize variables
 double num1 = 0;
 double num2 = 0;
 double result = 0;
 
 
-            //title
-            Console.WriteLine("--------------");
-            Console.WriteLine(" Calc program ");
-            Console.WriteLine("--------------");
+//title
+Console.WriteLine("--------------");
+Console.WriteLine(" Calc program ");
+Console.WriteLine("--------------");
 
 
-            // input
-            Console.Write("Enter Number 1: ");
-            num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Number 2: ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-            // the '\t' is optional
-            Console.WriteLine("Enter an option:");
-            Console.WriteLine("\t+ : add");
-            Console.WriteLine("\t- : subtract");
-            Console.WriteLine("\t* : multiply");
-            Console.WriteLine("\t/ : Divide");
-            // query for option
-            System.Console.Write("Enter an option: ");
+// input
+Console.Write("Enter Number 1: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter Number 2: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+// the '\t' is optional
+Console.WriteLine("Enter an option:");
+Console.WriteLine("\t+ : add");
+Console.WriteLine("\t- : subtract");
+Console.WriteLine("\t* : multiply");
+Console.WriteLine("\t/ : Divide");
+// query for option
+System.Console.Write("Enter an option: ");
 
 
-            // determines what mathematical operation to perform!
-            switch (Console.ReadLine());
-            {
-                case "+":
-                    result = num1 + num2;
-                    Console.WriteLine("it's: " + result);
-                    break;
-                case "-":
-                    result = num1 - num2;
-                    Console.WriteLine("it's: " + result);
-                    break;
-                case "*":
-                    result = num1 * num2;
-                    Console.WriteLine("it's: " + result);
-                    break;
-                case "/":
-                    result = num1 / num2;
-                    Console.WriteLine("it's: " + result);
-                    break;
-                // if none above are chosen, the default will perform!
-        default:
-                    Console.WriteLine("Thats not a valid option");
-                    break;
-            }
-            Console.Write("would you like to calculate again? (Y/N): ");
+// determines what mathematical operation to perform!
+switch (Console.ReadLine())
+{
+    case "+":
+        result = num1 + num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "-":
+        result = num1 - num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "*":
+        result = num1 * num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "/":
+        result = num1 / num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    // if none above are chosen, the default will perform!
+    default:
+        Console.WriteLine("Thats not a valid option");
+        break;
+    }
 
-        while (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase));
-#pragma warning restore CS8602; // Dereference of a possibly null reference.
+    Console.Write("would you like to calculate again? (Y/N): ");
+
+    while (Console.ReadLine().Equals("Y", StringComparison.CurrentCultureIgnoreCase)) ;
 
 
-        
-Console.WriteLine("Bye!");
+    Console.WriteLine("Bye!");
 
-        // less garble hopefully!
-        Console.ReadKey();
+    // less garble hopefully!
+    Console.ReadKey();
 
