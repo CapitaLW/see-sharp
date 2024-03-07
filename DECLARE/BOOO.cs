9e8f7f4bf2f12c88a35286a2e84ff36ba5a832a5
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-07 17:26:31 -0600

    new file:   DECLARE/BOOO.cs 	deleted:    DECLARE/Program.cs 	new file:   DECLARE/bin/Debug/net8.0/DECLARE.deps.json 	new file:   DECLARE/bin/Debug/net8.0/DECLARE.dll 	new file:   DECLARE/bin/Debug/net8.0/DECLARE.exe 	new file:   DECLARE/bin/Debug/net8.0/DECLARE.pdb 	new file:   DECLARE/bin/Debug/net8.0/DECLARE.runtimeconfig.json 	modified:   DECLARE/obj/Debug/net8.0/DECLARE.AssemblyInfo.cs 	modified:   DECLARE/obj/Debug/net8.0/DECLARE.AssemblyInfoInputs.cache 	modified:   DECLARE/obj/Debug/net8.0/DECLARE.csproj.FileListAbsolute.txt 	new file:   DECLARE/obj/Debug/net8.0/DECLARE.dll new file:   DECLARE/obj/Debug/net8.0/DECLARE.genruntimeconfig.cache 	new file:   DECLARE/obj/Debug/net8.0/DECLARE.pdb 	modified:   DECLARE/obj/Debug/net8.0/DECLARE.sourcelink.json 	new file:   DECLARE/obj/Debug/net8.0/apphost.exe 	new file:   DECLARE/obj/Debug/net8.0/ref/DECLARE.dll 	new file:   DECLARE/obj/Debug/net8.0/refint/DECLARE.dll

---
+++ DECLARE/BOOO.cs
@@ -0,0 +1,20 @@
+﻿int pushP = 0;
+string pluh = "p";
+
+Console.Write("increment by: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+while (pushP != 1000)
+{
+    if (Console.ReadLine() == pluh)
+    {
+        pushP += b;
+    }
+    Console.WriteLine("Incremented! " + pushP);
+}
+
+Console.ReadKey();
+
+
+// this took me a while to figure out, but I figured out how to do it.
+// hooray!
--- DECLARE/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int pushP = 0;
-string pluh = "P";
-
-if (Console.ReadLine() == pluh);
-{pushP ++ 1};
-
-Console.WriteLine(pushP);
-Console.ReadKez;
