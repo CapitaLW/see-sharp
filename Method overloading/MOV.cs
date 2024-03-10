dfeda174dddc0a57f354a9c47a65dc1910aaa622
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-10 18:37:07 -0500

    new file:   Method overloading/MOV.cs 	new file:   Method overloading/Method overloading.csproj 	new file:   Method overloading/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Method overloading/obj/Debug/net8.0/Method overloading.AssemblyInfo.cs 	new file:   Method overloading/obj/Debug/net8.0/Method overloading.AssemblyInfoInputs.cache 	new file:   Method overloading/obj/Debug/net8.0/Method overloading.GeneratedMSBuildEditorConfig.editorconfig 	new file:   Method overloading/obj/Debug/net8.0/Method overloading.GlobalUsings.g.cs 	renamed:    Quickpractice clac/Program.cs -> Quickpractice clac/another calculator.cs 	modified:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.AssemblyInfo.cs 	modified:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.AssemblyInfoInputs.cache 	deleted:    Ret Keywords/Program.cs 	new file:   Ret Keywords/Ret kw.cs 	new file:   Ret Keywords/bin/Debug/net8.0/Ret Keywords.deps.json 	new file:   Ret Keywords/bin/Debug/net8.0/Ret Keywords.dll 	new file:   Ret Keywords/bin/Debug/net8.0/Ret Keywords.exe 	new file:   Ret Keywords/bin/Debug/net8.0/Ret Keywords.pdb 	new file:   Ret Keywords/bin/Debug/net8.0/Ret Keywords.runtimeconfig.json 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.assets.cache 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.csproj.CoreCompileInputs.cache 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.csproj.FileListAbsolute.txt 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.dll 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.genruntimeconfig.cache 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.pdb 	new file:   Ret Keywords/obj/Debug/net8.0/Ret Keywords.sourcelink.json 	new file:   Ret Keywords/obj/Debug/net8.0/apphost.exe 	new file:   Ret Keywords/obj/Debug/net8.0/ref/Ret Keywords.dll 	new file:   Ret Keywords/obj/Debug/net8.0/refint/Ret Keywords.dll 	new file:   Ret Keywords/obj/Ret Keywords.csproj.nuget.dgspec.json 	new file:   Ret Keywords/obj/Ret Keywords.csproj.nuget.g.props 	new file:   Ret Keywords/obj/Ret Keywords.csproj.nuget.g.targets 	new file:   Ret Keywords/obj/project.assets.json 	new file:   Ret Keywords/obj/project.nuget.cache 	modified:   see sharp.sln

---
+++ Method overloading/MOV.cs
@@ -0,0 +1,2 @@
+﻿// method overloading
+
---
+++ Ret Keywords/Ret kw.cs
@@ -0,0 +1,19 @@
+﻿// return keywords
+Double x = 0;
+Double y = 0;
+Double result = 0;
+
+Console.Write("Enter number 1: ");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter number 2: ");
+y = Convert.ToDouble(Console.ReadLine());
+
+static Double multiply (Double x, Double y)
+{
+    Double z = x * y;
+    return z;
+}
+
+result = multiply(x, y);
+Console.WriteLine(result);
