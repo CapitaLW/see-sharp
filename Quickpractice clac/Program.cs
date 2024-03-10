f81446b50e9fd08d53b8333649a6772ff2aa6832
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-10 18:21:26 -0500

    modified:   DECLARE/obj/Debug/net8.0/DECLARE.AssemblyInfo.cs 	modified:   DECLARE/obj/Debug/net8.0/DECLARE.AssemblyInfoInputs.cache 	new file:   Quickpractice clac/Program.cs 	new file:   Quickpractice clac/Quickpractice clac.csproj 	new file:   Quickpractice clac/bin/Debug/net8.0/Quickpractice clac.deps.json 	new file:   Quickpractice clac/bin/Debug/net8.0/Quickpractice clac.dll 	new file:   Quickpractice clac/bin/Debug/net8.0/Quickpractice clac.exe 	new file:   Quickpractice clac/bin/Debug/net8.0/Quickpractice clac.pdb 	new file:   Quickpractice clac/bin/Debug/net8.0/Quickpractice clac.runtimeconfig.json 	new file:   Quickpractice clac/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.AssemblyInfo.cs 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.AssemblyInfoInputs.cache 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.GeneratedMSBuildEditorConfig.editorconfig#	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.GlobalUsings.g.cs 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.assets.cache 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.csproj.CoreCompileInputs.cache 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.csproj.FileListAbsolute.txt 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.dll 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.genruntimeconfig.cache 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.pdb 	new file:   Quickpractice clac/obj/Debug/net8.0/Quickpractice clac.sourcelink.json 	new file:   Quickpractice clac/obj/Debug/net8.0/apphost.exe 	new file:   Quickpractice clac/obj/Debug/net8.0/ref/Quickpractice clac.dll 	new file:   Quickpractice clac/obj/Debug/net8.0/refint/Quickpractice clac.dll 	new file:   Quickpractice clac/obj/Quickpractice clac.csproj.nuget.dgspec.json 	new file:   Quickpractice clac/obj/Quickpractice clac.csproj.nuget.g.props 	new file:   Quickpractice clac/obj/Quickpractice clac.csproj.nuget.g.targets 	new file:   Quickpractice clac/obj/project.assets.json 	new file:   Quickpractice clac/obj/project.nuget.cache 	modified:   THE METHODS/obj/Debug/net8.0/THE METHODS.AssemblyInfo.cs 	modified:   THE METHODS/obj/Debug/net8.0/THE METHODS.AssemblyInfoInputs.cache 	modified:   dacalc/bin/Debug/net8.0/dacalc.dll 	modified:   dacalc/bin/Debug/net8.0/dacalc.exe 	modified:   dacalc/bin/Debug/net8.0/dacalc.pdb 	modified:   dacalc/obj/Debug/net8.0/apphost.exe 	modified:   dacalc/obj/Debug/net8.0/dacalc.AssemblyInfo.cs 	modified:   dacalc/obj/Debug/net8.0/dacalc.AssemblyInfoInputs.cache 	modified:   dacalc/obj/Debug/net8.0/dacalc.dll 	modified:   dacalc/obj/Debug/net8.0/dacalc.pdb 	modified:   dacalc/obj/Debug/net8.0/dacalc.sourcelink.json 	modified:   dacalc/obj/Debug/net8.0/ref/dacalc.dll 	modified:   dacalc/obj/Debug/net8.0/refint/dacalc.dll 	modified:   see sharp.sln

---
+++ Quickpractice clac/Program.cs
@@ -0,0 +1,48 @@
+﻿// just for fun
+
+double num1 = 0;
+double num2 = 0;
+double result = 0;
+
+Console.WriteLine("-----------------------");
+Console.WriteLine("   Calculator program  ");
+Console.WriteLine("-----------------------");
+
+Console.Write("Enter number 1: ");
+num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter number 2: ");
+num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Multiply:");
+Console.WriteLine("Divide:");
+Console.WriteLine("Add:");
+Console.WriteLine("Subtract: ");
+Console.WriteLine("-------------------------------");
+
+switch (Console.ReadLine().ToLower())
+{
+    case "add":
+        result = num1 + num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "subtract":
+        result = num1 - num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "multiply":
+        result = num1 * num2;
+        Console.WriteLine("it's: " + result);
+        break;
+    case "divide":
+        result = num1 / num2;
+        Console.WriteLine("it's: " + result);
+        break;
+   
+    default:
+        Console.WriteLine("Thats not a valid option");
+        break;
+    }
+
+Console.WriteLine("Byee!");
+
+Console.ReadKey();
