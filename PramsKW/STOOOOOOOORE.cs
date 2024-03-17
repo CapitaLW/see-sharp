2fc2b4262cfd9710d0dab05470ce04f1d029b49e
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-17 14:04:02 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	deleted:    PramsKW/Program.cs
    	new file:   PramsKW/STOOOOOOOORE.cs
    	modified:   PramsKW/bin/Debug/net8.0/PramsKW.dll
    	modified:   PramsKW/bin/Debug/net8.0/PramsKW.exe
    	modified:   PramsKW/bin/Debug/net8.0/PramsKW.pdb
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfo.cs
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfoInputs.cache
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.csproj.CoreCompileInputs.cache
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.dll
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.pdb
    	modified:   PramsKW/obj/Debug/net8.0/PramsKW.sourcelink.json
    	modified:   PramsKW/obj/Debug/net8.0/apphost.exe
    	modified:   PramsKW/obj/Debug/net8.0/ref/PramsKW.dll
    	modified:   PramsKW/obj/Debug/net8.0/refint/PramsKW.dll

---
+++ PramsKW/STOOOOOOOORE.cs
@@ -0,0 +1,96 @@
+﻿// only needing a single method to accept a variable number of arguments
+Console.Clear();
+
+
+  Random balance = new Random();
+    double _ = 10 + (10000 - 10) * balance.NextDouble();
+    
+
+// cost to buy wat u want
+double TotalCost = 0;
+
+// balance after buying wat u want
+double PoorerBalance = Math.Round( _ - TotalCost );
+
+Console.WriteLine("---------------------------------------------------------------------------------------------------");
+Console.WriteLine("                                            kEWL stoRE!!!!                                         ");
+Console.WriteLine("                                            Buy Things!!!!                                         ");
+Console.WriteLine("---------------------------------------------------------------------------------------------------");
+Console.WriteLine("                                                                                                   ");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--SToRE--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine("|1: irradiated Tshirts 77.22   |2:    Pants 19.22 |3: Shoes 12.22    |4: Pluh mobile 10.22   | ");
+Console.WriteLine("|5: sus potion *Out of Stock*   |6:   Sponge 16.22 |7: Needles 1.22   |8:  Bacon 111.22       | ");
+Console.WriteLine("|9; omega turkey bacon 18.22   |10:  Eggs  14.22  |11: Pee Jouce 220 |12: Dababy car 10000   | ");
+Console.WriteLine("                              New items and restocking coming soon!                                ");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine("           What would you like to buy?   ~~ your current balance is " + "" + PoorerBalance + " ~~"         );
+Console.WriteLine("    type the item number of an item to purchase separate itemsusing commas or type EXIT to exit.   ");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.Write("Numbers go here ~~> ");
+
+
+(string name, double price)[] items =
+ {
+    ("Irradiated Tshirts", 77.22),
+    ("Pants", 19.22),
+    ("Shoes", 12.22),
+    ("Plub Mobile", 10.22),
+    ("Sus Potion", 0), // Out of stock
+    ("Sponge", 16.22),
+    ("Needles", 1.22),
+    ("Bacon", 111.22),
+    ("Omega Turkey Bacon", 18.22),
+    ("Eggs", 14.22),
+    ("Pee Jouce", 220),
+    ("Dababy Car", 10000)
+};
+
+while (true){
+ string? input = Console.ReadLine()?.Trim().ToLower();
+
+            if (input == "exit")
+            {
+                break;
+            }
+
+            string[] inputs = input?.Split(',') ?? new string[0];
+            foreach (string i in inputs)
+            {
+                if (int.TryParse(i.Trim(), out int selectedItemIndex) && selectedItemIndex >= 1 && selectedItemIndex <= items.Length)
+                {
+                    var selectedItem = items[selectedItemIndex - 1];
+
+                    if (selectedItem.price > 0)
+                    {
+                        if (Math.Round(PoorerBalance) >= selectedItem.price)
+                        {
+                            PoorerBalance -= selectedItem.price;
+                            Console.WriteLine($"You have purchased {selectedItem.name} for ${selectedItem.price}. Your new balance is ${Math.Round(PoorerBalance)}.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You cannot afford {selectedItem.name}. Your current balance is ${Math.Round(PoorerBalance)}.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{selectedItem.name} is currently out of stock.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid item number. Please select a number from the list.");
+                }
+            }
+        }
+
+
+Console.WriteLine("Your remaining balance is: " + Math.Round(PoorerBalance));
+Console.WriteLine("");
+
+Console.WriteLine("Thanks for shopping at kEWL stoRE!!!!");
+
+
+// PLUHHHH
+Console.ReadKey();
+// who up wonking they willy nilly???
--- PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfo.cs
+++ PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PramsKW")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ba332a852746cdacc2c9be1ca35ce8a48abc989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c1aeee52600f0a2c70d0d16782208eca4aecb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PramsKW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PramsKW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
