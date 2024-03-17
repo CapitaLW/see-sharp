d6c1aeee52600f0a2c70d0d16782208eca4aecb4
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-17 11:13:10 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	new file:   PramsKW.sln
    	new file:   PramsKW/PramsKW.csproj
    	new file:   PramsKW/Program.cs
    	new file:   PramsKW/bin/Debug/net8.0/PramsKW.deps.json
    	new file:   PramsKW/bin/Debug/net8.0/PramsKW.dll
    	new file:   PramsKW/bin/Debug/net8.0/PramsKW.exe
    	new file:   PramsKW/bin/Debug/net8.0/PramsKW.pdb
    	new file:   PramsKW/bin/Debug/net8.0/PramsKW.runtimeconfig.json
    	new file:   PramsKW/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfo.cs
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.AssemblyInfoInputs.cache
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.GlobalUsings.g.cs
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.assets.cache
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.csproj.CoreCompileInputs.cache
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.csproj.FileListAbsolute.txt
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.dll
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.genruntimeconfig.cache
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.pdb
    	new file:   PramsKW/obj/Debug/net8.0/PramsKW.sourcelink.json
    	new file:   PramsKW/obj/Debug/net8.0/apphost.exe
    	new file:   PramsKW/obj/Debug/net8.0/ref/PramsKW.dll
    	new file:   PramsKW/obj/Debug/net8.0/refint/PramsKW.dll
    	new file:   PramsKW/obj/PramsKW.csproj.nuget.dgspec.json
    	new file:   PramsKW/obj/PramsKW.csproj.nuget.g.props
    	new file:   PramsKW/obj/PramsKW.csproj.nuget.g.targets
    	new file:   PramsKW/obj/project.assets.json
    	new file:   PramsKW/obj/project.nuget.cache

---
+++ PramsKW/Program.cs
@@ -0,0 +1,31 @@
+﻿// only needing a single method to accept a variable number of arguments
+Console.Write("------------------------------------------------------------------------------------");
+Console.Write("                                       kEWL stoRE!!!!                               ");
+Console.Write("                                       Buy Things!!!!                               ");
+Console.Write("------------------------------------------------------------------------------------");
+Console.Write("                                                                                    ");
+Console.Write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~--SToRE--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+Console.Write(" irradiated Tshirts $77.22   |   Pants $14.22 | Shoes $12.22   | Pluh mobile $10.22 ");    
+Console.Write(" sus potion *Out of Stock*   |  Sponge $16.22 | Needles $14.22 | Bacon $16.22       ");     
+Console.Write(" omega turkey bacon $16.22   |  Eggs  $14.22  | Pee Jouce $220 | Dababy cars $10000 "); 
+
+
+double total = CheckOut(3.99, 5.75, 15);
+
+System.Console.WriteLine(total);
+
+static double CheckOut(params double[] prices)
+{
+double total = 0;
+
+    foreach (double price in prices)
+    {
+        total += price;
+    }
+
+    return total;
+}
+
+// PLUHHHH
+Console.ReadKey();
+// who up wonking they willy nilly???
