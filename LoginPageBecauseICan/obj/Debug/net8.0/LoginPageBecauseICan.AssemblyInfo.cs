7538d513872668724da4a5919ed4e69829cb073a
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-03-23 20:11:18 -0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   LoginPageBecauseICan/bin/Debug/net8.0/LoginPageBecauseICan.dll
    	modified:   LoginPageBecauseICan/bin/Debug/net8.0/LoginPageBecauseICan.exe
    	modified:   LoginPageBecauseICan/bin/Debug/net8.0/LoginPageBecauseICan.pdb
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.AssemblyInfo.cs
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.AssemblyInfoInputs.cache
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.csproj.CoreCompileInputs.cache
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.dll
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.pdb
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.sourcelink.json
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/apphost.exe
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/ref/LoginPageBecauseICan.dll
    	modified:   LoginPageBecauseICan/obj/Debug/net8.0/refint/LoginPageBecauseICan.dll

--- LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.AssemblyInfo.cs
+++ LoginPageBecauseICan/obj/Debug/net8.0/LoginPageBecauseICan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginPageBecauseICan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ca89b67db2bc04aaa850f54b41133a414f4662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13bd5bdf3aa68b601272e37aab53f391cb9790b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginPageBecauseICan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginPageBecauseICan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
