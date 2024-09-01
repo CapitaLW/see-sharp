7ee5ec9d5251a144ca556058bfe023cd9f2d43e2
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-09-01 15:38:00 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   LoginPageBecauseICan.sln
    	new file:   ccltr/Program.cs
    	new file:   ccltr/ccltr.csproj
    	new file:   ccltr/obj/Debug/net9.0/.NETCoreApp,Version=v9.0.AssemblyAttributes.cs
    	new file:   ccltr/obj/Debug/net9.0/ccltr.AssemblyInfo.cs
    	new file:   ccltr/obj/Debug/net9.0/ccltr.AssemblyInfoInputs.cache
    	new file:   ccltr/obj/Debug/net9.0/ccltr.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   ccltr/obj/Debug/net9.0/ccltr.GlobalUsings.g.cs
    	new file:   ccltr/obj/Debug/net9.0/ccltr.assets.cache
    	new file:   ccltr/obj/ccltr.csproj.nuget.dgspec.json
    	new file:   ccltr/obj/ccltr.csproj.nuget.g.props
    	new file:   ccltr/obj/ccltr.csproj.nuget.g.targets
    	new file:   ccltr/obj/project.assets.json
    	new file:   ccltr/obj/project.nuget.cache

---
+++ ccltr/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Calculate?");
+Console.WriteLine("   y    /     n   ");
+Console.ReadLine();
+
+Console.WriteLine("|----------------------------------------------------------------------|");
+Console.WriteLine("|                                      [ Cal -culator ]                                           |");
+Console.WriteLine("|       [ + ]                   [ - ]                        [ x ]                      [ / ]          |");
+Console.WriteLine("|      Add      |      Subtract      |      Multiply      |      Divide         |");
+Console.WriteLine("|                            [ Enter - An - Operation ]                                |");
+Console.WriteLine("|----------------------------------------------------------------------|");
