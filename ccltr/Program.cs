fdb6c99a06d67cf6471734794934e5d8a86bce87
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-09-01 15:58:18 -0400

    modified:   ccltr/Program.cs

--- ccltr/Program.cs
+++ ccltr/Program.cs
@@ -1,10 +1,28 @@
-﻿Console.WriteLine("Calculate?");
-Console.WriteLine("   y    /     n   ");
-Console.ReadLine();
+﻿Console.Clear();
+double num1 = 0;
+double num2 = 0;
+double num3 = 0;
 
+string iscalcing = string.Empty;
+
+while (string.IsNullOrWhiteSpace(iscalcing))
+{
+Console.WriteLine("Calculate?");
+Console.WriteLine("   y    /     n   ");
+iscalcing =  Console.ReadLine();
+}
+    
+
+while (true) 
+{
 Console.WriteLine("|----------------------------------------------------------------------|");
 Console.WriteLine("|                                      [ Cal -culator ]                                           |");
-Console.WriteLine("|       [ + ]                   [ - ]                        [ x ]                      [ / ]          |");
-Console.WriteLine("|      Add      |      Subtract      |      Multiply      |      Divide         |");
+Console.WriteLine("|      [ + ]                   [ - ]                       [ x ]                      [ / ]             |");
+Console.WriteLine("|      Add      |      Subtract      |      Multiply      |      Divide          |"); 
 Console.WriteLine("|                            [ Enter - An - Operation ]                                |");
 Console.WriteLine("|----------------------------------------------------------------------|");
+
+switch 
+{}
+
+}
