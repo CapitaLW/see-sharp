672a7a2c0f56f7b7ca10db13950354420a27a26c
ouggggggh <162051564+CapitaLW@users.noreply.github.com>
2024-09-04 18:32:43 -0400

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is ahead of 'origin/main' by 3 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	new file:   GUESS OR ELSE/GUESS OR ELSE.csproj
    	new file:   GUESS OR ELSE/Program.cs
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.AssemblyInfo.cs
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.AssemblyInfoInputs.cache
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.GlobalUsings.g.cs
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.assets.cache
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.csproj.CoreCompileInputs.cache
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.csproj.FileListAbsolute.txt
    	new file:   GUESS OR ELSE/obj/Debug/net8.0/GUESS OR ELSE.sourcelink.json
    	new file:   GUESS OR ELSE/obj/GUESS OR ELSE.csproj.nuget.dgspec.json
    	new file:   GUESS OR ELSE/obj/GUESS OR ELSE.csproj.nuget.g.props
    	new file:   GUESS OR ELSE/obj/GUESS OR ELSE.csproj.nuget.g.targets
    	new file:   GUESS OR ELSE/obj/project.assets.json
    	new file:   GUESS OR ELSE/obj/project.nuget.cache
    	modified:   LoginPageBecauseICan.sln

---
+++ GUESS OR ELSE/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+String presetPassword = "16924";
+String presetUsername = "PresTrack";
+String PasA = Strine.Empty;
+String UseA = Strine.Empty;
+bool p = true;
+bool u = true;
+
+Console.WriteLine("--------------------------------");
+Console.WriteLine("    Log into Track  ");
+Console.WriteLine("       Time Limit :       ");
+
+PasA = Console. ReadLine();
+If (PasA == presetPassword)
+{
+    continue;
+}
+else (if PasA != presetPassword)
+{
+    System.Console.WriteLine(" Wrong Password!");
+    p = false;
+}
+
+UseA = Console.ReadLine();
+if (UseA == presetUsername && p = true)
+{ 
+    continue;
+}
+else (if UseA != presetUsername)
+{
+    System.Console.WriteLine(" Incorrect Username or Password! ");
+}
+
+Console.WriteLine("  ");
+Console.WriteLine("  ");
+
+
